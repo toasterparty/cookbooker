4b7b52c69411aec16115619bc8ead6bd36bd4371
toasterparty <toasterparty@derpymail.org>
2023-05-07 11:48:28 -0700

    automatic configuration

--- tools/stop.sh
+++ tools/stop.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+TOP_DIR=$SCRIPT_DIR/..
 
-cd $SCRIPT_DIR/../supabase/docker
-sudo docker compose --env-file $SCRIPT_DIR/.env down
+cd $TOP_DIR/supabase/docker
+sudo docker compose --env-file $TOP_DIR/.env --env-file $TOP_DIR/config down
 sudo docker container prune -f

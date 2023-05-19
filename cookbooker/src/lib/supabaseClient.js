e1fff0e99ac714c50288676e24366b244fa0b7dc
toasterparty <toasterparty@derpymail.org>
2023-05-19 13:23:34 -0700

    use public-facing supabase

--- cookbooker/src/lib/supabaseClient.js
+++ cookbooker/src/lib/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 export const supabase = createClient(
-  'http://192.168.0.186:8000',
+  'https://supabase.toasterparty.net',
   'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.ewogICAgInJvbGUiOiAiYW5vbiIsCiAgICAiaXNzIjogInN1cGFiYXNlIiwKICAgICJpYXQiOiAxNjgzMzU2NDAwLAogICAgImV4cCI6IDE4NDEyMDkyMDAKfQ.9ADxq4L1Apzrq-TDmB7dTGwJGMN6jYDzZB_jfcH_NQU'
 )

e32b5ac0cc6ed7e6aa6705acbcaa53a3812968ee
toasterparty <toasterparty@derpymail.org>
2023-07-04 18:20:25 -0700

    Persist name change

--- cookbooker/src/views/recipe_edit.vue
+++ cookbooker/src/views/recipe_edit.vue
@@ -1,12 +1,58 @@
-<style></style>
+<style>
+ .recipe {
+    display: flex;
+    flex-direction: column;
+    align-items: left;
+  }
+
+  .custom-label {
+    margin-right: 10px;
+    font-size: 16px;
+  }
+
+  .custom-input {
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+    font-size: 16px;
+    width: 300px;
+    margin-bottom: 10px;
+  }
+
+  .button-container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-top: 10px;
+  }
+  
+  .custom-button {
+    background-color: #4CAF50;
+    color: white;
+    padding: 10px 20px;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    font-size: 16px;
+  }
+  
+  .custom-button:hover {
+    background-color: #45a049;
+  }
+</style>
 
 <template>
   <div class="recipe">
     <div v-if="recipe">
       <!-- Name -->
-      <input v-model="recipe.name" type="text" />
+      <div class="custom-input-container">
+        <label class="custom-label" for="recipe-name">Recipe Name</label>
+        <input class="custom-input" id="recipe-name" v-model="recipe.name" type="text" @input="updateRecipeName" />
+      </div>
       <!-- Save -->
-      <button @click="save_recipe">Save</button>
+      <div class="button-container">
+        <button class="custom-button" @click="save_recipe">Save</button>
+      </div>
     </div>
     <h1 v-else>⌛</h1>
   </div>
@@ -39,6 +85,9 @@ export default {
         console.error('Failed to save recipe:', error);
         alert('Failed to save recipe.');
       }
+    },
+    updateRecipeName(event) {
+      this.recipe.name = event.target.value;
     },
   },
 }
